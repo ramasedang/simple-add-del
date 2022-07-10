363a448e6ac3ff285e2270e7e65e83a855cbed90
ramasedang <ramasedang@gmail.com>
2022-07-10 18:07:52 +0700

    ssudo

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -16,7 +16,7 @@ getUserByID();
   const updateUser = async (e) => {
     e.preventDefault();
     try {
-      await axios.patch(`http://localhost:5000/users/${id}`, {
+      await axios.patch(`https://ssudo-api.herokuapp.com/users/${id}`, {
         name,
         gender,
         email,
@@ -28,7 +28,7 @@ getUserByID();
   };
 
   const getUserByID = async () => {
-    const response = await axios.get(`http://localhost:5000/users/${id}`);
+    const response = await axios.get(`https://ssudo-api.herokuapp.com/users/${id}`);
     setName(response.data.name);
     setEmail(response.data.email);
     setGender(response.data.gender);
